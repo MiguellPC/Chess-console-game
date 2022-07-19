b89483133ed6538e8278849107b3e32c51e71ea9
MiguellPC <miguelp326@gmail.com>
2022-07-18 23:15:43 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using tabuleiro;
+﻿using tabuleiro;
+using Pecas_xadrez;
 
 namespace xadrez_console
 {
@@ -25,6 +25,14 @@ namespace xadrez_console
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna = s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
         }
 
         public static void imprimirPeca(Peca peca)
